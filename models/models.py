9d6129dcfaaf763de05be205a0b16bd1b2ecde80
AFzOficial <abol.mortal50@gmail.com>
2023-07-03 18:02:38 +0330

    fix: username uniq in user model

--- models/models.py
+++ models/models.py
@@ -11,7 +11,7 @@ class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String)
+    username = Column(String, unique=True, index=True)
     password = Column(String)
     posts = relationship("Post", back_populates="creator")
 
